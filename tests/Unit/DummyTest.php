80950187efbc3dd81067040fd58bdcfcec070c46
Zoltán Borsos <zolli07@gmail.com>
2016-06-17 20:59:00 +0200

    Finsihed migrationg old data, added a little dummy test and reorganized test suite structure

---
+++ tests/Unit/DummyTest.php
@@ -0,0 +1,11 @@
+<?php namespace BuildR\Path\Tests\Unit;
+
+use BuildR\TestTools\BuildR_TestCase;
+
+class DummyTest extends BuildR_TestCase {
+
+    public function testDummy() {
+        $this->assertTrue(TRUE);
+    }
+
+}
